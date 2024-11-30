e91b472e5db31679cb40d28f9dd3e3c20e4b16dd
Kambolo <kamil.boloz0407@gmail.com>
2024-11-30 16:02:48 +0100

    Rejestracja z wykorzystaniem API, config Cors

---
+++ pk-hotel-backend/src/main/java/edu/zespol5/pkhotelbackend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package edu.zespol5.pkhotelbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@CrossOrigin(origins = "http://localhost:3000") // Allow specific origin for this controller
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000"); // Allow your frontend origin
+        config.addAllowedHeader("*"); // Allow all headers
+        config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
